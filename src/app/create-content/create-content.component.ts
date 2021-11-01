5134c7e27c8541cde7f06452bcc71e67dcd9b08b
skaurtech <w0746380@myscc.ca>
2021-11-01 17:34:02 -0400

    week 5 Assignment.

---
+++ src/app/create-content/create-content.component.ts
@@ -0,0 +1,54 @@
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
+import { Content } from '../helper-files/content-interface';
+
+@Component({
+  selector: 'app-create-content',
+  templateUrl: './create-content.component.html',
+  styleUrls: ['./create-content.component.css']
+})
+export class CreateContentComponent implements OnInit {
+@Output() newContentEvent = new EventEmitter<Content>();
+newContent: Content;
+
+error?: string;
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  onAddNews(
+    title: string,
+    body: string,
+    author: string,
+    imgUrl: string,
+    type: string,
+    tags: string
+  ) {
+    this.newContent = {
+      id: 0,
+      title: title,
+      body: body,
+      author: author,
+      imgUrl: imgUrl,
+      type: type,
+      tags: [tags]
+  };
+    let promiseToAddNews = new Promise((success, fail) => {
+      if (body && title && author) {
+        this.error = undefined;
+        this.newContentEvent.emit(this.newContent);
+        success(`News Added! - ${this.newContent.title}`);
+      } else {
+        this.error = `You need to add all required fields: ${
+          title ? '' : 'title'
+        } ${body ? '' : 'body'} ${author ? '' : 'author'}`;
+        fail(this.error);
+      }
+    });
+
+    promiseToAddNews
+      .then((successMessage) => console.log(successMessage))
+      .catch((failMessage) => console.log(failMessage));
+  }
+}
+
