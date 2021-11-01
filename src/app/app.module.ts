f97f56bd2cdabf1b9d7981f484e23ca3112050b9
skaurtech <w0746380@myscc.ca>
2021-11-01 17:27:08 -0400

    week 5 Assignment.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { ContentCardComponent } from './content-card/content-card.component';
 import { ContentListComponent } from './content-list/content-list.component';
 import { FilterContentPipe } from './filter-content.pipe';
 import { HoverStyleDirective } from './hover-style.directive';
+import { CreateContentComponent } from './create-content/create-content.component';
 
 @NgModule({
   declarations: [
@@ -13,7 +14,8 @@ import { HoverStyleDirective } from './hover-style.directive';
     ContentCardComponent,
     ContentListComponent,
     FilterContentPipe,
-    HoverStyleDirective
+    HoverStyleDirective,
+    CreateContentComponent
   ],
   imports: [
     BrowserModule
